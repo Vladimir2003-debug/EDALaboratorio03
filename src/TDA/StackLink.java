f7ddc10702f783a7eba91454dcbf60c4b42efccf
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-02 23:39:50 -0500

    añadido metodos de la interfaz

---
+++ src/TDA/StackLink.java
@@ -0,0 +1,35 @@
+package TDA;
+
+public class StackLink<E> implements TDAStack<E> {
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E push(E item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int search(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
