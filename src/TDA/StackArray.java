cc3a146fc41c0fa8b964e70174d268a9f88e24b1
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-02 23:48:55 -0500

    añadido constructores, comentarios descriptivos de los metodos

--- src/TDA/StackArray.java
+++ src/TDA/StackArray.java
@@ -1,32 +1,42 @@
 package TDA;
 
 public class StackArray<E> implements TDAStack<E> {
+    private Node<E> top;
 
-	@Override
+    //constructor emptyStack
+    public StackArray() {
+        this.top = null;
+    }
+
+    //constructor push top
+    public StackArray(E item) {
+        push(item);
+    }
+
+    //comprueba si la pila esta vacia
 	public boolean empty() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.top == null;
 	}
 
-	@Override
+    //mira la parte superior de la pila sin sacarlo de la pila
 	public E peek() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	//elimina el objeto en la aparte superior de la pila y devuelve el objeto como el valor de la funcion 
 	public E pop() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	//inserta un elemento a la parte superior de la pila
 	public E push(E item) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	//devuelve la pocicion basada en 1 donde se encuentre el objeto en la pila
 	public int search(Object o) {
 		// TODO Auto-generated method stub
 		return 0;
