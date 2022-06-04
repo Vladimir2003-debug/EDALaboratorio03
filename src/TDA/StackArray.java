fdf71dd8afd34b2e93757bfe8f5c0c6537cf76bb
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-04 09:49:42 -0500

    desarrollada la clase StackArray

--- src/TDA/StackArray.java
+++ src/TDA/StackArray.java
@@ -1,35 +1,90 @@
 package TDA;
 
-public class StackLink<E> implements TDAStack<E> {
+import java.util.EmptyStackException;
 
-	@Override
+public class StackArray<E> implements TDAStack<E> {
+	private E[] elements;
+	private E top;
+	
+	private final int defaultStackSize = 10;
+	/**
+	 * constructor
+	 */
+	public StackArray() {
+		this.elements = (E[]) new Object[defaultStackSize];
+		this.top = elements[0];
+	}
+
+	public StackArray(E element) {
+		push(element);
+	}
+	
 	public boolean empty() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.top == null;
 	}
 
-	@Override
 	public E peek() {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.top == null)
+			new EmptyStackException();
+		return this.top;
 	}
 
-	@Override
 	public E pop() {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.top == null)
+			new EmptyStackException();
+		E item = this.top;
+		return item;
 	}
 
-	@Override
 	public E push(E item) {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.top == null) {
+			this.top = item;
+			elements[0] = item;
+			return item;
+		}
+		
+		int i = elements.length - 1;
+		while(elements[i] == null) {
+			i--;
+		}
+		if ((i) == elements.length - 1) {
+			rezize();
+		}
+		elements[i+1] = item;
+		this.top = elements[i];
+		return item;
 	}
-
-	@Override
+	
+	/**
+	 * Metodo que copia los elementos de un arreglo
+	 * a otro de tamaño doble
+	 */
+	public void rezize() {
+		int length = elements.length;
+		E[] tmp= (E[]) new Object[ length * 2];
+		for (int i = 0; i < length; i++) {
+			tmp[i] = elements[i];
+		}
+		elements = tmp;
+	}
+	
 	public int search(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		int i = 0;
+		while(elements[i] != null) {
+			if(elements[i].equals(o))
+				return i;
+			i++;
+		}
+		return -1;
+	}
+	
+	public String toString() {
+		String txt = "";
+		for (int i = 0; i < elements.length; i++) {
+			if(elements[i] == null) continue;
+			txt += elements[i] + " "; 
+		}
+		return txt;
 	}
 
 }
