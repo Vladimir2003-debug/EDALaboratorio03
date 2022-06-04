c6b13fae998e47b7aeafcf4a51bd9fbd35f25ff6
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-04 11:51:23 -0500

    desarrollado metodos de queuearray, sin evaluar

---
+++ src/TDA/QueueArray.java
@@ -0,0 +1,35 @@
+package TDA;
+
+public class QueueArray<E> implements TDAQueue<E> {
+
+	public boolean add(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public E element() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean offer(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public E poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public E remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
+}
