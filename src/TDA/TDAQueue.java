8ea7f112577e1fc6b453f95f55481b5f36c3d7ed
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-05 20:12:52 -0500

    añadido public para que sea utilizable fuera del package

--- src/TDA/TDAQueue.java
+++ src/TDA/TDAQueue.java
@@ -1,34 +1,34 @@
 package TDA;
-interface TDAQueue<E> {
+public interface TDAQueue<E> {
 	
 	/**
 	 * Ingresa el elemento en la cola de acuedo a los limites
 	 * de capacidad  
-	 * @param e elemento a añadir
+	 * @param e elemento a aï¿½adir
 	 * @return true cuando el elemento ha sido ingresado
-	 * @IllegalStateException Si el elemento no se puede agregar debido a las restricciones de capacidad
-     * @ClassCastException Si la clase impide que se agrege a la cola
-     * @NullPointerException Si el elemento es nulo y la cola no permite elementos nulos
-     * @IllegalArgumentException Si alguna propiedad del elemento impide que se agrege a la cola
+	 * @throws IllegalStateException Si el elemento no se puede agregar debido a las restricciones de capacidad
+     * @throws ClassCastException Si la clase impide que se agrege a la cola
+     * @throws NullPointerException Si el elemento es nulo y la cola no permite elementos nulos
+     * @throws IllegalArgumentException Si alguna propiedad del elemento impide que se agrege a la cola
 	 */
     boolean add(E e);
     
     /**
      * Devuelve el elemento head(cabeza) de la cola
      * @return head La cabeza de la cola
-     * @NoSuchElementException Si la cola esta vacia 
+     * @throws NoSuchElementException Si la cola esta vacia 
      */
     E element();
     
     /**
      * Inserta el elemento en la cola. Este metodo a diferencia de add
      * inserta elementos a pesar de la restriccion de capacidad
-     * @param e Elemento a añadir
-     * @return true Si el elemento se añadio a la cola
-     * @return false Si el elemento no se añadio a la cola
-     * @ClassCastException Si la clase del elemento impide que se agrege a esta cola
-     * @NullPointerException Si el elemento es nulo y la cola no permite elementos nulos
-     * @IllegalArgumentException Si alguna propiedad de este elemento impide que se agrege a esta cola
+     * @param e Elemento a aï¿½adir
+     * @return true Si el elemento se aï¿½adio a la cola
+     * @return false Si el elemento no se aï¿½adio a la cola
+     * @throws ClassCastException Si la clase del elemento impide que se agrege a esta cola
+     * @throws NullPointerException Si el elemento es nulo y la cola no permite elementos nulos
+     * @throws IllegalArgumentException Si alguna propiedad de este elemento impide que se agrege a esta cola
      */
     boolean offer(E e);
     
@@ -50,7 +50,7 @@ interface TDAQueue<E> {
      * Recupera y elimina la cabeza de la cola. A diferencia de poll lanzara una
      *  exception si la cola esta vacia
      * @return head La cabeza de la cola
-     * @NoSuchElementException Si la cola esta vacia
+     * @throws NoSuchElementException Si la cola esta vacia
      */
     E remove();
 }
