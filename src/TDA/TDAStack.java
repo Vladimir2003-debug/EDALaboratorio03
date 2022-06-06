1554cb1975877b652d130eedad6ffdde58d42b4a
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-05 20:13:24 -0500

    añadido public para su visualizacin en otro package

--- src/TDA/TDAStack.java
+++ src/TDA/TDAStack.java
@@ -1,27 +1,31 @@
 package TDA;
-interface TDAStack<E> {
+public interface TDAStack<E> {
 	/**
-	 * comprueba si la pila esta vacia
+	 * Comprueba si la pila esta vacia
+	 * @return True si esta vacia
+	 * @return False si esta con algun objeto
 	 */
 	boolean empty();
     /**
-     * mira la parte superior de la pila sin sacarlo de la pila
+     * Mira la parte superior de la pila sin sacarlo de la pila
+     * @return el objeto top en la cima de la pila 
+     * @throws EmptyStackException si la pila esta vacia
      */
     E peek();
     /**
-     * elimina el objeto en la aparte superior de la pila y devuelve el objeto como el valor de la funcion
-     * @exception si el TDAStack esta vacio  
+     * Elimina el objeto en la aparte superior de la pila y devuelve el objeto como el valor de la funcion
+     * @return El objeto al tope de la lista
+     * @throws EmptyStackException si el TDAStack esta vacio  
      */
     E pop();
 	/**
-	 * inserta un elemento a la parte superior de la pila
+	 * Inserta un elemento a la parte superior de la pila
 	 * @param item valor generico que se desea ingrear a la pila
 	 * @return item el valor que se desea ingresar
-	 * @exception si el TDAStack esta vacio
 	 */
     E push(E item);
     /**
-     * devuelve la pocicion basada en 1 donde se encuentre el objeto en la pila
+     * Devuelve la pocicion basada en 1 donde se encuentre el objeto en la pila
      * @param o el objeto deseado
      * @return la pocicion basada en 1 si retorna -1 el objeto no esta en la pila
      */
